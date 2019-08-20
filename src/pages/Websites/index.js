8e309e66cdd40b118624e1203b27e2fa93dc24db
ron881212 <ron881212@gmail.com>
2019-08-19 20:28:55 -0400

    completed hero and enter layout for web tab

--- src/pages/Websites/index.js
+++ src/pages/Websites/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import textImg from '../../images/webAppImg02.png'
 import './styles.css'
 
 // This page contains large hero image, cards, buttons, and maybe redux or card info
@@ -6,12 +7,15 @@ import './styles.css'
 const Websites = () => {
 
     return(
-        <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-              <p className="jumbo-text d-flex justify-content-center">Web Apps</p>
-              <p className="lead d-flex justify-content-center">Text info will be here</p>
+        <div className="container">
+            <img src={textImg} alt='code' className="img-fluid"/>
+            <h2 className='jumbo-text text-center'>All Web Apps below are full stack</h2>
+            <p className='text-center info-text'>Click Below to see Projects</p>
+            <div className="d-flex justify-content-center">
+            <button type="button" className="btn btn-primary btn-lg rounded-circle start-btn d-flex justify-content-center"><i className="fas fa-desktop fa-2x "></i></button>
             </div>
         </div>
+
     )
 }
 
