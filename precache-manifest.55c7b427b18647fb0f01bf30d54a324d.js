054a16aa50d9ba19d544e346e13e987c693abc39
Ronald Glover <ron881212@gmail.com>
2019-09-30 17:54:13 -0400

    Updates

---
+++ precache-manifest.55c7b427b18647fb0f01bf30d54a324d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d781157366f96f37a710c243493543ee",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "e43f3c95dfa45186c9d6",
+    "url": "/reactPortfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cc3de6680dead2fc8d2e",
+    "url": "/reactPortfolio/static/css/main.4d29c291.chunk.css"
+  },
+  {
+    "revision": "e43f3c95dfa45186c9d6",
+    "url": "/reactPortfolio/static/js/2.7d956816.chunk.js"
+  },
+  {
+    "revision": "cc3de6680dead2fc8d2e",
+    "url": "/reactPortfolio/static/js/main.be6f3078.chunk.js"
+  },
+  {
+    "revision": "8c32832a2b4cffefe4c2",
+    "url": "/reactPortfolio/static/js/runtime~main.bce5e2f1.js"
+  }
+]);
