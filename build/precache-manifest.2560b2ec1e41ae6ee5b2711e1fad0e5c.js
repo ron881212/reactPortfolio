65705f171bbbcbbf4664a3d42c80f6f6f1310e70
Ronald Glover <ron881212@gmail.com>
2020-04-16 11:08:04 -0400

    Update static

---
+++ build/precache-manifest.2560b2ec1e41ae6ee5b2711e1fad0e5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82de7f510eb58bc8a3a462893879cbac",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "4ae10e495f3ea9b74f22",
+    "url": "/reactPortfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "958806689da2ecd29020",
+    "url": "/reactPortfolio/static/css/main.31528f62.chunk.css"
+  },
+  {
+    "revision": "4ae10e495f3ea9b74f22",
+    "url": "/reactPortfolio/static/js/2.9b16b6ba.chunk.js"
+  },
+  {
+    "revision": "958806689da2ecd29020",
+    "url": "/reactPortfolio/static/js/main.4c8d8e21.chunk.js"
+  },
+  {
+    "revision": "353436343a1c5dc6c1ed",
+    "url": "/reactPortfolio/static/js/runtime~main.c1f5fe1e.js"
+  },
+  {
+    "revision": "fbf422e39612877a3d39cad3a80d5746",
+    "url": "/reactPortfolio/static/media/contactMe05.fbf422e3.png"
+  },
+  {
+    "revision": "81d3403c46b0644a37737e83fd75834c",
+    "url": "/reactPortfolio/static/media/profilePic01.81d3403c.png"
+  },
+  {
+    "revision": "3f0c9901ce2273acfb8e4fb3742abb4f",
+    "url": "/reactPortfolio/static/media/webAppImg02.3f0c9901.png"
+  }
+]);
